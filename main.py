e8f2b6c182b25a250ac1057308f39d8d1420eab0
Russell Stadler <3325739+rstad@users.noreply.github.com>
2023-01-18 03:01:15 +0000

    Attempts to use server nicks in primary server
    Didn't really go all the way for addressing #1 but instead just attempts
    to use nicks when avaialble from one of the servers.
    
    Also minor tweak to .gitignore

--- main.py
+++ main.py
@@ -12,99 +12,108 @@ padline = "❌❌❌❌❌"
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 client = discord.Client(intents=intents)
 
+def get_server_nick(server,author):
+    try:
+        member = server.get_member(author.id)
+        if member.display_name:
+            return member.display_name
+        else:
+            return author.name
+    except:
+        return author.name
+
 def transform_message(message):
-    author = message.author.name
-    #print(author)
-    if re.search(rex,message.content).start() == 0: # no starting word, straight wordle paste
-        if re.search(fivegreens,message.content) != None: # not X/6
-            if re.search(botrex,message.content) != None: # has wordlebot scores
-                formatted_message = author + '\n'
-                header = message.content.split('\n')[0]
+    if re.search(rex,message).start() == 0: # no starting word, straight wordle paste
+        if re.search(fivegreens,message) != None: # not X/6
+            if re.search(botrex,message) != None: # has wordlebot scores
+                formatted_message = ""
+                header = message.split('\n')[0]
                 slindex = header.find("/")
                 formatted_message += header[:slindex-2]+" ||"+header[slindex-1]+"||"+header[slindex:]+'\n\n'
-                formatted_message += "||"+re.split(rex,message.content)[1].split('\n\n')[1].split(fivegreens)[0]
+                formatted_message += "||"+re.split(rex,message)[1].split('\n\n')[1].split(fivegreens)[0]
                 formatted_message += fivegreens+"\n"
-                for i in range(5-len(re.split(rex,message.content)[1].split('\n\n')[1].split(fivegreens)[0].strip().split('\n'))):
+                for i in range(5-len(re.split(rex,message)[1].split('\n\n')[1].split(fivegreens)[0].strip().split('\n'))):
                     formatted_message+=padline+'\n'
                 formatted_message += '||\nWordleBot\n'
-                formatted_message += re.split(rex,message.content)[1].split('\n\nWordleBot\n')[1]
+                formatted_message += re.split(rex,message)[1].split('\n\nWordleBot\n')[1]
                 return formatted_message
             else: # straight paste, solved, no wordlebot
-                formatted_message = author + '\n'
-                header = message.content.split('\n')[0]
+                formatted_message = ""
+                header = message.split('\n')[0]
                 slindex = header.find("/")
                 formatted_message += header[:slindex-2]+" ||"+header[slindex-1]+"||"+header[slindex:]+'\n\n'
-                formatted_message += "||"+re.split(rex,message.content)[1].split('\n\n')[1].split(fivegreens)[0]
+                formatted_message += "||"+re.split(rex,message)[1].split('\n\n')[1].split(fivegreens)[0]
                 formatted_message += fivegreens+"\n"
-                for i in range(5-len(re.split(rex,message.content)[1].split('\n\n')[1].split(fivegreens)[0].strip().split('\n'))):
+                for i in range(5-len(re.split(rex,message)[1].split('\n\n')[1].split(fivegreens)[0].strip().split('\n'))):
                     formatted_message+=padline+'\n'
                 formatted_message = formatted_message.strip()
-                formatted_message += re.split(rex,message.content)[1].split('\n\n')[1].split(fivegreens)[1]+"||"
+                formatted_message += re.split(rex,message)[1].split('\n\n')[1].split(fivegreens)[1]+"||"
                 return formatted_message
         else: # straight paste, failed
-            if re.search(botrex,message.content) != None: # has wordlebot scores
-                formatted_message = author + '\n'
-                header = message.content.split('\n')[0]
+            if re.search(botrex,message) != None: # has wordlebot scores
+                formatted_message = ""
+                header = message.split('\n')[0]
                 slindex = header.find("/")
                 formatted_message += header[:slindex-2]+" ||"+header[slindex-1]+"||"+header[slindex:]+'\n\n'
-                formatted_message += "||"+re.split(rex,message.content)[1].split('\n\n')[1]+"||"
+                formatted_message += "||"+re.split(rex,message)[1].split('\n\n')[1]+"||"
                 formatted_message += '\n\nWordleBot\n'
-                formatted_message += re.split(rex,message.content)[1].split('\n\nWordleBot\n')[1]
+                formatted_message += re.split(rex,message)[1].split('\n\nWordleBot\n')[1]
                 return formatted_message
             else: # no wordlebot score, straight paste, failed
-                formatted_message = author + '\n'
-                header = message.content.split('\n')[0]
+                formatted_message = ""
+                header = message.split('\n')[0]
                 slindex = header.find("/")
                 formatted_message += header[:slindex-2]+" ||"+header[slindex-1]+"||"+header[slindex:]+'\n\n'
-                formatted_message += "||"+re.split(rex,message.content)[1].split('\n\n')[1]+"||"
+                formatted_message += "||"+re.split(rex,message)[1].split('\n\n')[1]+"||"
                 return formatted_message
     else: # has a leading word
-        if re.search(fivegreens,message.content) != None: # not X/6
-            if re.search(botrex,message.content) != None: # has wordlebot scores
-                formatted_message = author + " " + re.split(rex,message.content)[0].strip().upper()+'\n'
-                header = message.content.split('\n')[0]
+        if re.search(fivegreens,message) != None: # not X/6
+            if re.search(botrex,message) != None: # has wordlebot scores
+                formatted_message = re.split(rex,message)[0].strip().upper()+'\n'
+                header = message.split('\n')[0]
                 slindex = header.find("/")
                 windex = header.find("Wordle")
                 formatted_message += header[windex:slindex-2]+" ||"+header[slindex-1]+"||"+header[slindex:]+'\n\n'
-                formatted_message += "||"+re.split(rex,message.content)[1].split('\n\n')[1].split(fivegreens)[0]
+                formatted_message += "||"+re.split(rex,message)[1].split('\n\n')[1].split(fivegreens)[0]
                 formatted_message += fivegreens+"\n"
-                for i in range(5-len(re.split(rex,message.content)[1].split('\n\n')[1].split(fivegreens)[0].strip().split('\n'))):
+                for i in range(5-len(re.split(rex,message)[1].split('\n\n')[1].split(fivegreens)[0].strip().split('\n'))):
                     formatted_message+=padline+'\n'
                 formatted_message += '||\nWordleBot\n'
-                formatted_message += re.split(rex,message.content)[1].split('\n\nWordleBot\n')[1]
+                formatted_message += re.split(rex,message)[1].split('\n\nWordleBot\n')[1]
                 return formatted_message
             else:
-                formatted_message = author + " " + re.split(rex,message.content)[0].strip().upper()+'\n'
-                header = message.content.split('\n')[0]
+                formatted_message = re.split(rex,message)[0].strip().upper()+'\n'
+                header = message.split('\n')[0]
                 slindex = header.find("/")
                 windex = header.find("Wordle")
                 formatted_message += header[windex:slindex-2]+" ||"+header[slindex-1]+"||"+header[slindex:]+'\n\n'
-                formatted_message += "||"+re.split(rex,message.content)[1].split('\n\n')[1]+'\n'
-                for i in range(5-len(re.split(rex,message.content)[1].split('\n\n')[1].split(fivegreens)[0].strip().split('\n'))):
+                formatted_message += "||"+re.split(rex,message)[1].split('\n\n')[1]+'\n'
+                for i in range(5-len(re.split(rex,message)[1].split('\n\n')[1].split(fivegreens)[0].strip().split('\n'))):
                     formatted_message+=padline+'\n'
                 formatted_message = formatted_message.strip()+"||"
                 return formatted_message
         else:
-            if re.search(botrex,message.content) != None: # has wordlebot scores
-                formatted_message = author + " " + re.split(rex,message.content)[0].strip().upper()+'\n'
-                header = message.content.split('\n')[0]
+            if re.search(botrex,message) != None: # has wordlebot scores
+                formatted_message = re.split(rex,message)[0].strip().upper()+'\n'
+                header = message.split('\n')[0]
                 slindex = header.find("/")
                 windex = header.find("Wordle")
                 formatted_message += header[windex:slindex-2]+" ||"+header[slindex-1]+"||"+header[slindex:]+'\n\n'
-                formatted_message += "||"+re.split(rex,message.content)[1].split('\n\n')[1]+"||"
+                formatted_message += "||"+re.split(rex,message)[1].split('\n\n')[1]+"||"
                 formatted_message += '\n\nWordleBot\n'
-                formatted_message += re.split(rex,message.content)[1].split('\n\nWordleBot\n')[1]
+                formatted_message += re.split(rex,message)[1].split('\n\nWordleBot\n')[1]
                 return formatted_message
             else: # failed, so no padding
-                formatted_message = author + " " + re.split(rex,message.content)[0].strip().upper()+'\n'
-                header = message.content.split('\n')[0]
+                formatted_message = re.split(rex,message)[0].strip().upper()+'\n'
+                header = message.split('\n')[0]
                 slindex = header.find("/")
                 windex = header.find("Wordle")
                 formatted_message += header[windex:slindex-2]+" ||"+header[slindex-1]+"||"+header[slindex:]+'\n\n'
-                formatted_message += "||"+re.split(rex,message.content)[1].split('\n\n')[1]+"||"
+                formatted_message += "||"+re.split(rex,message)[1].split('\n\n')[1]+"||"
                 return formatted_message
 
 def is_wordle_message(message):
@@ -124,28 +133,29 @@ async def on_message(message):
     #print(str(message.content))
     if is_wordle_message(message.content):
         print("is wordle message")
-        formatted_message = transform_message(message)
-        #print(formatted_message)
+        formatted_message = transform_message(message.content)
         fmh_wordle = client.get_channel(1064641280791019623)
         rta_general = client.get_channel(692795745409171589)
+        rta_guild = client.get_guild(692795745409171586)
+        print(message)
+        print(message.content)
+        print(get_server_nick(rta_guild,message.author))
+        print(formatted_message)
         if not message.guild: # is DM?
             try:
-                await fmh_wordle.send(formatted_message)
-                await rta_general.send(formatted_message)
-                pass
+                await rta_general.send(get_server_nick(rta_guild,message.author)+" "+formatted_message)
+                await fmh_wordle.send(get_server_nick(rta_guild,message.author)+" "+formatted_message)
             except discord.errors.Forbidden:
                 pass
         else: # not DM
             if message.channel.id == 1064641280791019623: #fmh wordle
                 try:
-                    await rta_general.send(formatted_message)
-                    pass
+                    await rta_general.send(get_server_nick(rta_guild,message.author)+" "+formatted_message)
                 except discord.errors.Forbidden:
                     pass
             if message.channel.id == 692795745409171589: #rt2a general
                 try:
-                    await fmh_wordle.send(formatted_message)
-                    pass
+                    await fmh_wordle.send(get_server_nick(rta_guild,message.author)+" "+formatted_message)
                 except discord.errors.Forbidden:
                     pass
     else:
